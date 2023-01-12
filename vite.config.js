55853f461bcc6bf24908d8f70e0c920d862b42b1
Felipe <feferreira.sf@gmail.com>
2023-01-12 17:54:30 -0300

    test multi page

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,14 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'desafios/player/index.html'),
-        nested: resolve(__dirname, 'desafios/product-card/index.html')
+        nested: resolve(
+          __dirname,
+          'desafios/player/index.html',
+          'desafios/teste3/index.html',
+          'desafios/product-card/index.html'
+        )
+        // nested: resolve(__dirname, 'desafios/teste3/index.html'),
+        // nested: resolve(__dirname, 'desafios/product-card/index.html')
       }
     }
   }
