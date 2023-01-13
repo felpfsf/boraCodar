89187a6a06ecf80cf57873e38c0100319be11a67
Felipe <feferreira.sf@gmail.com>
2023-01-12 22:21:24 -0300

    deploy vite vercel  images issue

--- desafios/product-card/js/main.js
+++ desafios/product-card/js/main.js
@@ -7,11 +7,11 @@ let toggle = true
 function onToggleImage() {
   toggle = !toggle
   if (toggle) {
-    prodImg.src = './public/images/couch-static.png'
-    buttonImg.src = './public/images/360-vector.png'
+    prodImg.src = '../../public/images/product-card/couch-static.png'
+    buttonImg.src = '../../public/images/product-card/360-vector.png'
   } else {
-    buttonImg.src = './public/images/close.png'
-    prodImg.src = './public/images/couch-360.gif'
+    buttonImg.src = '../../public/images/product-card/close.png'
+    prodImg.src = '../../public/images/product-card/couch-360.gif'
   }
 }
 
